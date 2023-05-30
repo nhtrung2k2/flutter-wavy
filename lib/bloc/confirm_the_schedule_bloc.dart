2a5a9ec5148ac7abc2429d092d20a8801e2a4698
Thanh Bui <thanhbpc@voyager-hcm.com>
2023-05-30 16:35:42 +0700

    some fix at confirm schedule bloc

--- lib/bloc/confirm_the_schedule_bloc.dart
+++ lib/bloc/confirm_the_schedule_bloc.dart
@@ -8,14 +8,12 @@ import 'package:wavy/repository/employees_repository.dart';
 import 'package:wavy/service/getit/service_locator.dart';
 import 'package:wavy/state/confirm_the_schedule_state.dart';
 
-class ConfirmTheScheduleBloc
-    extends Bloc<ConfirmScheduleEvent, ConfirmTheScheduleState> {
-  final EmployeesRepository _employeesRepository;
-  final ConfirmTheScheduleRepository _confirmTheScheduleRepository =
-      ServiceLocator.locator.get<ConfirmTheScheduleRepository>();
+class ConfirmTheScheduleBloc extends Bloc<ConfirmScheduleEvent, ConfirmTheScheduleState> {
 
-  ConfirmTheScheduleBloc(this._employeesRepository)
-      : super(ConfirmTheScheduleState.initial()) {
+  final EmployeesRepository _employeesRepository;
+  final ConfirmTheScheduleRepository _confirmTheScheduleRepository = ServiceLocator.locator.get<ConfirmTheScheduleRepository>();
+
+  ConfirmTheScheduleBloc(this._employeesRepository) : super(ConfirmTheScheduleState.initial()) {
     on<LoadDataConfirmScheduleEvent>(_onLoadData);
     on<NextMonthEvent>(_onNextMonth);
     on<PrevMonthEvent>(_onPrevMonth);
@@ -23,88 +21,100 @@ class ConfirmTheScheduleBloc
   }
 
   Future<void> _onLoadData(
-    LoadDataConfirmScheduleEvent event,
-    Emitter<ConfirmTheScheduleState> emit,
-  ) async {
+      LoadDataConfirmScheduleEvent event,
+      Emitter<ConfirmTheScheduleState> emit,
+      ) async {
+
     emit(state.copyWith(
-        month: DateTime.now().month,
         year: DateTime.now().year,
+        month: DateTime.now().month,
         informationStatus: InformationStatus.loading,
-        monthScheduleStatus: MonthScheduleStatus.loading));
+        monthScheduleStatus: MonthScheduleStatus.loading
+    ));
 
     try {
-      Employee_Detail employeeDetail = await _employeesRepository
-          .fetchEmployDetail(event.babysisterId, null);
-      List<ScheduleConfirm> scheduleConfirms =
-          await _confirmTheScheduleRepository.fetchScheduleConfirm(
-              event.shiftId,
-              DateFormat('yyyy-MM').format(DateTime(state.year, state.month)));
+      Employee_Detail employeeDetail = await _employeesRepository.fetchEmployDetail(event.babysisterId, event.shiftId);
+      List<ScheduleConfirm> scheduleConfirms = await _confirmTheScheduleRepository.fetchScheduleConfirm(event.shiftId, DateFormat('yyyy-MM').format(DateTime(state.year, state.month)));
       emit(state.copyWith(
           employee: employeeDetail,
           scheduleConfirms: scheduleConfirms,
           informationStatus: InformationStatus.success,
-          monthScheduleStatus: MonthScheduleStatus.success));
+          monthScheduleStatus: MonthScheduleStatus.success
+      ));
     } catch (e) {
       emit(state.copyWith(
           informationStatus: InformationStatus.failure,
-          monthScheduleStatus: MonthScheduleStatus.failure));
+          monthScheduleStatus: MonthScheduleStatus.failure
+      ));
     }
+
   }
 
   Future<void> _onNextMonth(
-    NextMonthEvent event,
-    Emitter<ConfirmTheScheduleState> emit,
-  ) async {
+      NextMonthEvent event,
+      Emitter<ConfirmTheScheduleState> emit,
+      ) async {
+
     emit(state.nextMonth());
 
     try {
-      List<ScheduleConfirm> scheduleConfirms =
-          await _confirmTheScheduleRepository.fetchScheduleConfirm(
-              event.shiftId,
-              DateFormat('yyyy-MM').format(DateTime(state.year, state.month)));
+      List<ScheduleConfirm> scheduleConfirms = await _confirmTheScheduleRepository.fetchScheduleConfirm(event.shiftId, DateFormat('yyyy-MM').format(DateTime(state.year, state.month)));
       emit(state.copyWith(
           scheduleConfirms: scheduleConfirms,
-          monthScheduleStatus: MonthScheduleStatus.success));
+          monthScheduleStatus: MonthScheduleStatus.success
+      ));
     } catch (_) {
-      emit(state.copyWith(monthScheduleStatus: MonthScheduleStatus.failure));
+      emit(state.copyWith(
+          monthScheduleStatus: MonthScheduleStatus.failure
+      ));
     }
+
   }
 
   Future<void> _onPrevMonth(
-    PrevMonthEvent event,
-    Emitter<ConfirmTheScheduleState> emit,
-  ) async {
+      PrevMonthEvent event,
+      Emitter<ConfirmTheScheduleState> emit,
+      ) async {
+
     emit(state.prevMonth());
 
     try {
-      List<ScheduleConfirm> scheduleConfirms =
-          await _confirmTheScheduleRepository.fetchScheduleConfirm(
-              event.shiftId,
-              DateFormat('yyyy-MM').format(DateTime(state.year, state.month)));
+      List<ScheduleConfirm> scheduleConfirms = await _confirmTheScheduleRepository.fetchScheduleConfirm(event.shiftId, DateFormat('yyyy-MM').format(DateTime(state.year, state.month)));
       emit(state.copyWith(
           scheduleConfirms: scheduleConfirms,
-          monthScheduleStatus: MonthScheduleStatus.success));
+          monthScheduleStatus: MonthScheduleStatus.success
+      ));
     } catch (_) {
-      emit(state.copyWith(monthScheduleStatus: MonthScheduleStatus.failure));
+      emit(state.copyWith(
+          monthScheduleStatus: MonthScheduleStatus.failure
+      ));
     }
+
   }
 
   Future<void> _onCheckItem(
-    CheckItemEvent event,
-    Emitter<ConfirmTheScheduleState> emit,
-  ) async {
-    String message =
-        await _confirmTheScheduleRepository.updateConfirmFlag(event.amountId);
+      CheckItemEvent event,
+      Emitter<ConfirmTheScheduleState> emit,
+      ) async {
+
+    String message = await _confirmTheScheduleRepository.updateConfirmFlag(event.amountId);
 
     List<ScheduleConfirm> confirms = [];
-    for (ScheduleConfirm s in state.scheduleConfirms) {
-      if (s.amountId == event.amountId) {
-        confirms.add(s.copyWith(confirmFlag: event.value ? 1 : 0));
-      } else {
+    for(ScheduleConfirm s in state.scheduleConfirms){
+      if(s.amountId == event.amountId){
+        confirms.add(s.copyWith(
+            confirmFlag: event.value ? 1 : 0
+        ));
+      }
+      else{
         confirms.add(s);
       }
     }
 
-    emit(state.copyWith(scheduleConfirms: confirms));
+    emit(state.copyWith(
+        scheduleConfirms: confirms
+    ));
+
   }
+
 }
